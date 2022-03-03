cf6a04a2fcfb43efc4de67e5a171e1b879dfa458
ahmads1990 <ahmads20002000@gmail.com>
2022-03-04 01:18:23 +0200

    recycler view now working

--- app/src/main/java/com/example/simplebank/MainActivity.java
+++ app/src/main/java/com/example/simplebank/MainActivity.java
@@ -1,14 +1,43 @@
 package com.example.simplebank;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import com.example.simplebank.mainpage.recyclerListAdapter;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ArrayList<User> userArrayList = new ArrayList<>();
+        userArrayList.add(new User("ahmad","$",123457.5f));
+        userArrayList.add(new User("ahmad","$",123457.5f));
+        userArrayList.add(new User("ahmad","$",123457.5f));
+        userArrayList.add(new User("ahmad","$",123457.5f));
+        userArrayList.add(new User("ahmad","$",123457.5f));
+        userArrayList.add(new User("ahmad","$",123457.5f));
+
+        Log.d("mytag","us " + userArrayList.get(0).getUsername());
+        RecyclerView recyclerView = findViewById(R.id.main_user_list_recyler);
+        recyclerListAdapter adapter = new recyclerListAdapter(this, userArrayList);
+
+        recyclerView.setAdapter(adapter);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        /*
+                        RecyclerView recyclerView = findViewById(R.id.main_recycler);
+                //
+                viewAdapter adapter = new viewAdapter(mangaArrayList, context);
+                recyclerView.setAdapter(adapter);
+                recyclerView.setLayoutManager(new LinearLayoutManager(context));
+         */
     }
 }
---
+++ app/src/main/java/com/example/simplebank/mainpage/recyclerListAdapter.java
@@ -0,0 +1,83 @@
+package com.example.simplebank.mainpage;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.constraintlayout.widget.ConstraintLayout;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.simplebank.R;
+import com.example.simplebank.User;
+
+import java.util.ArrayList;
+
+public class recyclerListAdapter extends  RecyclerView.Adapter<recyclerListAdapter.ViewHolder> {
+
+    //data
+    private Context context;
+    private ArrayList<User> localDataSet;
+
+    public recyclerListAdapter(Context context, ArrayList<User> localDataSet) {
+        this.context = context;
+        this.localDataSet = localDataSet;
+    }
+
+    @NonNull
+    @Override
+    public recyclerListAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        Log.d("mytag", "onBindViewHolder: called.");
+        // Create a new view, which defines the UI of the list item
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.user_list_view_item, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull recyclerListAdapter.ViewHolder holder, int position) {
+        holder.name.setText(localDataSet.get(position).getUsername());
+        holder.email.setText(localDataSet.get(position).getEmail());
+        holder.balance.setText(String.valueOf(localDataSet.get(position).getCurrentBalance()));
+
+        holder.parentLayout.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        Toast.makeText(context, "the "+ localDataSet.get(holder.getAdapterPosition()).getUsername() ,Toast.LENGTH_SHORT).show();
+                    }
+                }
+        );
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return localDataSet.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name;
+        TextView email;
+        TextView balance;
+
+        ConstraintLayout parentLayout;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+
+            name = (TextView) itemView.findViewById(R.id.user_list_item_name);
+            email = (TextView) itemView.findViewById(R.id.user_list_item_email);
+            balance = (TextView) itemView.findViewById(R.id.user_list_item_balance);
+
+            parentLayout = (ConstraintLayout) itemView.findViewById(R.id.user_list_item_layout);
+        }
+
+    }
+}
